3732a7e4bf660a2e07961334f838744b7755a677
Sohaib Ahrich <soahrich@c1r2p1.1337.ma>
2026-09-16 18:18:10 +0100

    some tests

--- test.py
+++ test.py
@@ -73,13 +73,93 @@ import matplotlib.pyplot as plt
 # df = pd.DataFrame(data)
 # print(df[df["salary"] < 5005]["age"])
 
+# import pandas as pd
+
+
+# data = {
+#     "name": ["Ali", "Sara", "Omar", "Adam", "Lina", "Yassine"],
+#     "city": ["Casa", "Rabat", "Casa", "Rabat", "Casa", "Rabat"],
+#     "age": [28, 25, 35, 29, 38, 32],
+#     "salary": [1200, 1000, 2300, 1400, 2500, 2000]
+# }
+
+# df = pd.read_csv('products.csv')
+# # df = pd.DataFrame(data)
+
+# # counts = df["Product name"].value_counts().head(2)
+
+# # print(df.sort_values("First price", ascending=False)["First price"])
+
+
+# print(df.groupby("Category")["First price"].max())
+
+
 import pandas as pd
+import numpy as np
 
 data = {
-    "name": ["Ali", "Sara", "Omar", "Adam", "Lina"],
-    "age": [28, 25, 35, 29, 38],
-    "salary": [1200, 1000, 2300, 1400, 2500],
+    "product": [
+        "Laptop", "Phone", "Laptop",
+        "Mouse", "Phone", "Keyboard",
+        "Laptop", "Mouse"
+    ],
+
+    "category": [
+        "Computer", "Mobile", "Computer",
+        "Accessory", "Mobile", "Accessory",
+        "Computer", "Accessory"
+    ],
+
+    "price": [
+        1000, 600, 1200,
+        30, 650, 80,
+        np.nan, 25
+    ],
+
+    "quantity": [
+        2, 5, 1,
+        10, 3, 7,
+        2, 15
+    ]
 }
 
 df = pd.DataFrame(data)
 
+print("1. Print the first 5 rows.")
+print(df.head())
+
+print("2 Check how many missing values each column has")
+print(df.isnull().sum())
+
+print("3. Find the average price.")
+print(df["price"].mean())
+
+print("4. Find how many times each product appears.")
+count = df["product"].value_counts()
+print(count)
+
+print("5. Show only products that appear more than once.")
+# morethatOne = count[count > 1].index
+# for i in df.loc[df["product"].isin(morethatOne), "product"].unique():
+#     print(i)
+print(count[count > 1])
+
+print("6. Show rows where price is greater than 500.")
+print(df[df["price"] > 500])
+
+print("7. Sort products from highest price to lowest.")
+print(df.sort_values("price", ascending=False))
+
+print("8. Find the average price for each category.")
+print(df.groupby("category")["price"].mean())
+
+print("9. Find the total quantity for each category")
+print(df.groupby("category")["quantity"].sum())
+
+
+print("""
+10. ⭐ Create a new column:
+total_value = price × quantity
+""")
+df["total_value"] = df["price"] * df["quantity"]
+print(df)
